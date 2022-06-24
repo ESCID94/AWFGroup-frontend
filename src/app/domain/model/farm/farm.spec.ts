8dd727e0d96dc9a1c71e37ed9ae6cf35fdc0044a
Enrique Salazar del Cid <kike15j@gmail.com>
2022-06-24 14:10:06 +0200

    feat: added new model structure

---
+++ src/app/domain/model/farm/farm.spec.ts
@@ -0,0 +1,7 @@
+import { Farm } from './farm';
+
+describe('Farm', () => {
+  it('should create an instance', () => {
+    expect(new Farm()).toBeTruthy();
+  });
+});
---
+++ src/app/domain/model/sale/sale.spec.ts
@@ -0,0 +1,7 @@
+import { Sale } from './sale';
+
+describe('Sale', () => {
+  it('should create an instance', () => {
+    expect(new Sale()).toBeTruthy();
+  });
+});
